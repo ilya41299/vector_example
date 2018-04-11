d1ceed038d67126246c3393246cf3f8218b1efd7
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-04-11 20:16:43 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -5,13 +5,13 @@
 
 TEST_CASE("Сreating tree")
 {
-	tree_t My_tree;
+	tree_t<int> My_tree;
 	REQUIRE( My_tree.root() == nullptr );
 }
 
 TEST_CASE("Add elements in tree")
 {
-	tree_t My_tree;
+	tree_t<int> My_tree;
 	My_tree.insert(7);
 	My_tree.insert(3);
 	My_tree.insert(9);
@@ -28,7 +28,7 @@ TEST_CASE("Add elements in tree")
 
 TEST_CASE("Check root tree")
 {
-	tree_t My_tree;
+  tree_t<int> My_tree;
   My_tree.insert(7);
   My_tree.insert(3);
   My_tree.insert(9); 
@@ -39,7 +39,7 @@ TEST_CASE("Check root tree")
 
 TEST_CASE("Check operator tree '+' and '=' ")
 {
-	tree_t My_tree;
+	tree_t<int> My_tree;
 	std::ostringstream ostream;
 	My_tree.check_operator(ostream, '+', 7);
 	My_tree.check_operator(ostream, '+', 3);
@@ -56,7 +56,7 @@ TEST_CASE("Check operator tree '+' and '=' ")
 }
 TEST_CASE("Check operator tree '?' ")
 {
-	tree_t My_tree;
+	tree_t<int> My_tree;
 	My_tree.insert(7);
 	My_tree.insert(3);
 	My_tree.insert(9); 
